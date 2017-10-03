f10e80297b13419f1d5fb67976a3a8128c3bcad6
William Hilton <williamjchilton@gmail.com>
2017-10-03 15:15:37 +0100

    deployed chainlist contract

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -9,18 +9,18 @@ contract ChainList {
 
   // sell an article
   function sellArticle(string _name, string _description, uint256 _price) public {
-      seller = msg.sender;
-      name = _name;
-      description = _description;
-      price = _price;
+    seller = msg.sender;
+    name = _name;
+    description = _description;
+    price = _price;
   }
 
   // get the article
   function getArticle() public constant returns (
-      address _seller,
-      string _name,
-      string _description,
-      uint256 _price) {
-      return(seller, name, description, price);
+    address _seller,
+    string _name,
+    string _description,
+    uint256 _price) {
+    return(seller, name, description, price);
   }
 }
