ff7be064825f85abbfd9bedb98487a5e5c53dc4a
William Hilton <williamjchilton@gmail.com>
2017-10-03 18:10:29 +0100

    update appJS with connecting frontend with the smart contract

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 App = {
   web3Provider: null,
   contracts: {},
+  account: 0x0,
 
   init: function() {
     return App.initWeb3();
@@ -39,10 +40,46 @@ App = {
       // get the necessay contract artifact file and use it to instantiate a truffle contract abstraction.
       App.contracts.ChainList = TruffleContract(chainListArtifact);
       // Set the provider for out contract
-      App.contracts.ChainList = setProvider(App.web3Provider);
+      App.contracts.ChainList.setProvider(App.web3Provider);
+      return App.reloadArticles();
     });
   },
 
+  reloadArticles: function() {
+    // refresh account information because the balance my have changed
+    App.displayAccountInfo();
+
+    App.contracts.ChainList.deployed().then(function(instance) {
+      return instance.getArticle.call();
+    }).then(function(article) {
+      if (article[0] == 0x0) {
+        // no article
+        return;
+      }
+
+      // Retrieve and clear the articel placeholder
+      var articleRow = $('#articleRow');
+      articleRow.empty();
+
+      // Retrieve and fill article template
+      var articleTemplate = $('#articleTemplate');
+      articleTemplate.find('.panel-title').text(article[1]);
+      articleTemplate.find('.article-description').text(article[2]);
+      articleTemplate.find('.article-price').text(web3.fromWei(article[3], "ether"));
+
+      var seller = article[0];
+      if (seller == App.account) {
+        seller = "You";
+      }
+
+      articleTemplate.find('.article-seller').text(seller);
+
+      // add this new article
+      articleRow.append(articleTemplate.html());
+    }).catch(function(err) {
+      console.log(err.message);
+    });
+  },
 };
 
 $(function() {
