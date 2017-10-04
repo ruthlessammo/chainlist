ada2e723b7f2f3ce2fbe5c4487a26494ce486db1
William Hilton <williamjchilton@gmail.com>
2017-10-04 17:44:42 +0100

    added buyArticle functionality need to test in consol

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.11;
 contract ChainList {
   // State variables
   address seller;
+  address buyer;
   string name;
   string description;
   uint256 price;
@@ -14,7 +15,14 @@ contract ChainList {
     uint256 _price
   );
 
-  // sell an article
+  event buyArticleEvent (
+    address indexed _seller,
+    address indexed _buyer,
+    string _name,
+    uint256 _price
+  );
+
+  // Sell an article
   function sellArticle(string _name, string _description, uint256 _price) public {
     seller = msg.sender;
     name = _name;
@@ -26,9 +34,28 @@ contract ChainList {
   // get the article
   function getArticle() public constant returns (
     address _seller,
+    address _buyer,
     string _name,
     string _description,
     uint256 _price) {
-    return(seller, name, description, price);
+    return(seller, buyer, name, description, price);
+  }
+
+  // Buy article
+  function buyArticle() payable public {
+    // check whether there is an article for sale
+    require(seller != 0x0);
+    // check that the article is not already sold
+    require(buyer == 0x0);
+    // dont allow the seller to buy his own article
+    require(msg.sender != seller);
+    // check whether the value sent corresponds to the article price
+    require(msg.sender == price);
+    // keep buyers information
+    buyer = msg.sender;
+    //the buyer can buy the article
+    seller.transfer(msg.value);
+    //trigger the event
+    buyArticleEvent(sellerm buyer, name, price);
   }
 }
